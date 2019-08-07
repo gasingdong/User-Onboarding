8d2ea4b6adb5c681b0ca6f6ea62a009243fa43ca
TheIllusiveC4 <gasingdong@gmail.com>
2019-08-07 15:37:48 -0700

    add carbon design package

--- user-onboarding/src/App.tsx
+++ user-onboarding/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './App.css';
+import './App.scss';
 import FormikLoginForm from './components/LoginForm';
 import UserCard from './components/UserCard';
 
