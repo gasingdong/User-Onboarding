c743537aa34a1f7869e66e06cf9c536033b2e60a
TheIllusiveC4 <gasingdong@gmail.com>
2019-08-07 13:32:29 -0700

    add form validation for all fields

--- user-onboarding/src/components/LoginForm.tsx
+++ user-onboarding/src/components/LoginForm.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { withFormik, Form, Field, FormikProps } from 'formik';
+import * as Yup from 'yup';
 
 interface LoginValues {
   name: string;
@@ -17,12 +18,20 @@ interface LoginProps {
 
 const LoginForm = ({
   values,
+  touched,
+  errors,
 }: FormikProps<LoginValues>): React.ReactElement => {
   return (
     <Form>
+      {touched.name && errors.name && <p className="error">{errors.name}</p>}
       <Field type="name" name="name" placeholder="Name" />
+      {touched.email && errors.email && <p className="error">{errors.email}</p>}
       <Field type="email" name="email" placeholder="Email" />
+      {touched.password && errors.password && (
+        <p className="error">{errors.password}</p>
+      )}
       <Field type="password" name="password" placeholder="Password" />
+      {touched.tos && errors.tos && <p className="error">{errors.tos}</p>}
       <label className="checkbox-container">
         Accept Terms of Service
         <Field type="checkbox" name="tos" checked={values.tos} />
@@ -47,7 +56,22 @@ const FormikLoginForm = withFormik<LoginProps, LoginValues>({
     };
   },
 
-  handleSubmit(values, { setStatus }): void {},
+  validationSchema: Yup.object().shape({
+    name: Yup.string().required('Please enter a name'),
+    email: Yup.string()
+      .email('Please enter a valid email address')
+      .required('Please enter a email address'),
+    password: Yup.string()
+      .min(8, 'Password must be at least 8 characters')
+      .required(),
+    tos: Yup.boolean()
+      .oneOf([true], 'Please accept the Terms of Service')
+      .required(),
+  }),
+
+  handleSubmit(values, { setStatus }): void {
+    console.log(values);
+  },
 })(LoginForm);
 
 export default FormikLoginForm;
